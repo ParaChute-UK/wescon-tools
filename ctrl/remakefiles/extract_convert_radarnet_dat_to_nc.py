1a86cdaea535b26f59c4b0780e9dcac9726852fc
Mark Muetzelfeldt <markmuetz@gmail.com>
2026-02-27 17:16:01 +0000

    nimrod -> radarnet.

--- ctrl/remakefiles/extract_convert_radarnet_dat_to_nc.py
+++ ctrl/remakefiles/extract_convert_radarnet_dat_to_nc.py
@@ -13,12 +13,12 @@ from remake.util import sysrun
 
 from wescon_tools.radarnet_composite import RadarNetComposite, RadarNetDataReadError
 from wescon_tools import util
-from proj_config import PATHS, CASES, KASBEX_CASES
+from wescon_tools.proj_config import PATHS, CASES, KASBEX_CASES
 
 # Access has been suspended. I can still download each file through the web interface tho ¯\_(ツ)_/¯
 BADC_DATADIR = Path('/badc/ukmo-nimrod/data/composite/uk-1km/')
 # BADC_DATADIR = Path('/gws/nopw/j04/mcs_prime/mmuetz/upflo/data/nimrod/raw')
-OUTDIR = PATHS['datadir'] / 'nimrod'
+OUTDIR = PATHS['datadir'] / 'radarnet'
 
 slurm_config = {'account': 'mcs_prime', 'partition': 'standard', 'qos': 'standard', 'mem': 64000}
 rmk = Remake(config=dict(slurm=slurm_config))
